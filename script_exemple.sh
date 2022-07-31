926bd7fbb16d3c1c03e548f951df6f82841dc16b
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-07-31 15:53:18 -0300

    add shell script notes and shell script example

---
+++ script_exemple.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Creating system users....."
+
+useradd guest10 -c "Guest user" -s /bin/bash -m -p $(openssl passwd Password123)
+passwd guest10 -e 
+
+useradd guest11 -c "Guest user" -s /bin/bash -m -p $(openssl passwd Password123)
+passwd guest11 -e 
+
+useradd guest12 -c "Guest user" -s /bin/bash -m -p $(openssl passwd Password123)
+passwd guest12 -e 
+
+useradd guest13 -c "Guest user" -s /bin/bash -m -p $(openssl passwd Password123)
+passwd guest13 -e 
+
+echo "Complete!!"
